fc51154ecd75a5860f009abac3d7dc1c6c972cc0
徐怀远 <406892700@qq.com>
2018-08-30 17:20:41 +0800

    feat hehehehe

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,19 @@
-console.log('validating...')
-console.log(process.argv)
-setTimeout(() => {
-  process.exit(0)
-}, 2000)
+const fs = require('fs')
+const path = require('path')
+const standardMsgPrefix = [
+  'feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore'
+]
+// git文件目录
+const gitPath = path.resolve(__dirname, '../.git/')
+// 提交信息
+const commitMsg = fs.readFileSync(path.join(gitPath, 'COMMIT_EDITMSG')).toString().trim()
+
+const length = standardMsgPrefix.length
+for (let i = 0; i < length; i++) {
+  const reg = new RegExp('^'+standardMsgPrefix[i]+'[\s]{1}')
+  if (reg.test(commitMsg)) {
+    process.exit(0)
+  } else {
+    process.exit(1)
+  }
+}
